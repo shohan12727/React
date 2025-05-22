d6b7322af8932f0f997bf09c085d6e669474e713
shohan12727 <aishohan001@gmail.com>
2025-05-22 21:49:21 +0600

    counter revise done

--- React-1/src/App.jsx
+++ React-1/src/App.jsx
@@ -1,15 +1,40 @@
-import './App.css'
-import Shohan from './Shohan'
+import { useState } from "react";
+import "./App.css";
+import Shohan from "./Shohan";
 
 function App() {
-  
+  const [count, setCount] = useState(0);
 
+  const handleIncrement = () => {
+    setCount(count + 1)
+  }
+  const handleDecrement = () => {
+    setCount(count - 1)
+  }
+  const handleReset = () => {
+    setCount(0)
+  }
   return (
     <>
       <h1>KING SHOHAN 👑</h1>
       <Shohan></Shohan>
+      <h2>Counter Value </h2>
+      <div>
+        <button onClick={handleIncrement}>
+          Increment
+        </button>
+        <button>
+               Counter: {count}
+        </button>
+        <button onClick={handleDecrement}>
+          Decrement
+        </button>
+      </div>
+      <button onClick={handleReset}>
+        Reset
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
