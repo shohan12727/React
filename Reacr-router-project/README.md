d7fddcba5ec82114a7672da8994d4c7bccd38040
shohan12727 <aishohan001@gmail.com>
2025-06-19 11:29:52 +0600

    initialization of react router project

---
+++ Reacr-router-project/README.md
@@ -0,0 +1,2 @@
+# React-Router-SimpleProject
+A basic React project demonstrating how to use React Router to create multi-page navigation within a single-page application (SPA).
---
+++ Reacr-router-project/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+     <h1 className='text-2xl text-center bg-amber-200'>King Shohan👑</h1>
+    </>
+  )
+}
+
+export default App
+ 
